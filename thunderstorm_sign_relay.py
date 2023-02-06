379390c1216a14ff0c6050e9685d3dcb7715056a
Joshua S <joshua.ss11@gmail.com>
2023-02-06 17:39:03 +1100

    added time check

--- thunderstorm_sign_relay.py
+++ thunderstorm_sign_relay.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import requests
+from datetime import datetime
 
 #If code is stopped while the solenoid is active it stays active
 #This may produce a warning if the code is restarted and it finds the GPIO Pin, which it defines as non-active in next line, is still active
 #from previous time the code was run. This line prevents that warning syntax popping up which if it did would stop the code running.
-GPIO.cleanup()
+
 GPIO.setwarnings(True)
 #This means we will refer to the GPIO pins
 #by the number directly after the word GPIO. A good Pin Out Resource can be found here https://pinout.xyz/
@@ -19,7 +20,11 @@ headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KH
 response = requests.get("http://www.bom.gov.au/vic/forecasts/melbourne.shtml", headers=headers)
 web_text = response.text
 
-if "thunderstorm" in web_text:
+start_hour = 8
+end_hour = 18
+now = datetime.now()
+
+if "thunderstorm" in web_text and now.hour >= start_hour and now.hour <= end_hour:
     #turn on relay
     GPIO.output(21, 1)
 else:
